5e305a79ea7d1ef43f23df6d5a061658b74d0c1e
DanDavidovich <davidovich.dan@gmail.com>
2023-12-12 13:53:47 +0200

    loader componnent

--- frontend/src/components/loader/loader.jsx
+++ frontend/src/components/loader/loader.jsx
@@ -1,1 +1,14 @@
-//enter text here
+import React from "react";
+import {CircularProgress} from "@mui/material";
+
+
+const Loader = () => {
+    return(
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <CircularProgress />
+    </div>
+);
+
+}
+
+export default Loader;
