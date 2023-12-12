b90ff044dd1e344c02bc6bfa20626a9f30e7f3a9
Idan Asayag <idanasayag0@gmail.com>
2023-12-12 19:03:50 +0200

    upload icons

--- frontend/src/components/songCard/songCard.jsx
+++ frontend/src/components/songCard/songCard.jsx
@@ -4,6 +4,8 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
+import CommentIcon from "@mui/icons-material/Comment";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 
 export default function MultiActionAreaCard() {
   return (
@@ -61,13 +63,33 @@ export default function MultiActionAreaCard() {
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="success">
-          Commands: 15
-        </Button>
-        <Button size="small" color="primary">
-          Dowloanded: 7
-        </Button>
+      <CardActions
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          sx={{
+            color: "#6A6A6A",
+            fontWeight: 200,
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <CommentIcon sx={{ color: "#6A6A6A" }} /> 15
+        </Typography>
+        <Typography
+          sx={{
+            color: "#6A6A6A",
+            fontWeight: 200,
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <CloudDownloadIcon sx={{ color: "#6A6A6A" }} />7
+        </Typography>
       </CardActions>
     </Card>
   );
