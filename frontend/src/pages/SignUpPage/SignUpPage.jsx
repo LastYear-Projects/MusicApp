6111be88c93f570d4c8de674922e8b6608db9437
DanDavidovich <davidovich.dan@gmail.com>
2023-12-14 20:33:28 +0200

    confirm,redirect,required,timeout

--- frontend/src/pages/SignUpPage/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage/SignUpPage.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { Form, Input, DatePicker, Upload, Button, message } from 'antd';
-import { UserOutlined, InboxOutlined } from '@ant-design/icons';
-import { useForm } from 'react-hook-form';
+import React, {useState} from 'react';
+import {Form, Input, DatePicker, Upload, Button, message} from 'antd';
+import {UserOutlined, InboxOutlined} from '@ant-design/icons';
+import {useForm} from 'react-hook-form';
 
-const { Dragger } = Upload;
+const {Dragger} = Upload;
 
 const Signup = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const {register, handleSubmit, watch, formState: {errors}} = useForm();
     const [formData, setFormData] = useState({
         email: '',
         fullName: '',
@@ -25,15 +25,19 @@ const Signup = () => {
             return;
         }
         if (formData.password !== formData.cPassword) {
-            console.log('Passwords do not match');
+            message.error('Passwords do not match');
             return;
         }
+        else {
+            console.log('Email:', formData.email);
+            console.log('Full Name:', formData.fullName);
+            console.log('Password:', formData.password);
+            console.log('Confirmed Password:', formData.cPassword);
+            message.success('Signup successful!');
 
-        console.log('Email:', formData.email);
-        console.log('Full Name:', formData.fullName);
-        console.log('Password:', formData.password);
-        console.log('Confirmed Password:', formData.cPassword);
-        message.success('Signup successful!');
+            await new Promise(resolve => setTimeout(resolve, 3000));
+            window.location.href = 'http://localhost:3000';
+        }
     };
 
     const normFile = (e) => {
@@ -51,15 +55,15 @@ const Signup = () => {
     };
 
     return (
-        <div style={{ maxWidth: '500px', margin: 'auto' }}>
-            <h2 style={{ marginBottom: '1rem', textAlign: 'center', margin: 20, marginTop: 20 }}>Sign Up</h2>
+        <div style={{maxWidth: '500px', margin: 'auto'}}>
+            <h2 style={{marginBottom: '1rem', textAlign: 'center', margin: 20, marginTop: 20}}>Sign Up</h2>
             <Form onFinish={handleSubmit(onSubmit)}>
                 <Form.Item
                     name="email"
-                    rules={[{ required: true, message: 'Please input your email!' }]}
+                    rules={[{required: true, message: 'Please input your email!'}]}
                 >
                     <Input
-                        prefix={<UserOutlined />}
+                        prefix={<UserOutlined/>}
                         placeholder="Email"
                         {...register('email')}
                         onChange={(e) => handleInputChange('email', e.target.value)}
@@ -67,10 +71,10 @@ const Signup = () => {
                 </Form.Item>
                 <Form.Item
                     name="fullName"
-                    rules={[{ required: true, message: 'Please input your full name!' }]}
+                    rules={[{required: true, message: 'Please input your full name!'}]}
                 >
                     <Input
-                        prefix={<UserOutlined />}
+                        prefix={<UserOutlined/>}
                         placeholder="Full Name"
                         {...register('fullName')}
                         onChange={(e) => handleInputChange('fullName', e.target.value)}
@@ -78,11 +82,11 @@ const Signup = () => {
                 </Form.Item>
                 <Form.Item
                     name="dateOfBirth"
-                    rules={[{ required: true, message: 'Please select your date of birth!' }]}
+                    rules={[{required: true, message: 'Please select your date of birth!'}]}
                 >
                     <DatePicker
                         placeholder="Date of Birth"
-                        style={{ width: '100%' }}
+                        style={{width: '100%'}}
                         {...register('dateOfBirth')}
                         onChange={(date) => handleInputChange('dateOfBirth', date)}
                     />
@@ -91,11 +95,11 @@ const Signup = () => {
                     name="profilePicture"
                     valuePropName="fileList"
                     getValueFromEvent={normFile}
-                    rules={[{ required: false, message: 'Please upload your profile picture!' }]}
+                    rules={[{required: false, message: 'Please upload your profile picture!'}]}
                 >
                     <Dragger name="file" multiple={false} beforeUpload={() => false}>
                         <p className="ant-upload-drag-icon">
-                            <InboxOutlined />
+                            <InboxOutlined/>
                         </p>
                         <p className="ant-upload-text">Click or drag file to this area to upload</p>
                     </Dragger>
@@ -103,8 +107,7 @@ const Signup = () => {
                 <Form.Item
                     name="password"
                     rules={[{ required: true, message: 'Please input your password!' }]}
-                    // hasFeedback
-                    help={errors.password && errors.password.message}
+                    hasFeedback
                 >
                     <Input.Password
                         prefix={<UserOutlined />}
@@ -117,16 +120,16 @@ const Signup = () => {
                     name="cPassword"
                     rules={[
                         { required: true, message: 'Please confirm your password!' },
-                        { validator: (_, value) => value === watch('password') || 'Passwords do not match' },
+                        ({ getFieldValue }) => ({
+                            validator(_, value) {
+                                if (!value || getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+                                return Promise.reject(new Error('Passwords do not match'));
+                            },
+                        }),
                     ]}
-                    // hasFeedback
-                    help={
-                        errors.cPassword && (
-                            <span style={{ color: 'red', display: 'block', marginTop: '5px' }}>
-                                {errors.cPassword.message}
-                            </span>
-                        )
-                    }
+                    hasFeedback
                 >
                     <Input.Password
                         prefix={<UserOutlined />}
@@ -137,8 +140,8 @@ const Signup = () => {
                 </Form.Item>
 
                 <Form.Item>
-                    <Button type="primary" htmlType="submit" style={{ width: '100%' }}
-                    onClick={onSubmit}>
+                    <Button type="primary" htmlType="submit" style={{width: '100%'}}
+                            onClick={onSubmit}>
                         Sign Up
                     </Button>
                 </Form.Item>
