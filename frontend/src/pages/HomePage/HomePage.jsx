1838159c002893c8c7e61032cd2ae4fc9b31d1ed
Idan Asayag <idanasayag0@gmail.com>
2023-12-15 15:31:06 +0200

    remove playground from homepage

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,33 +1,7 @@
 import React from "react";
-import List from "../../components/list/List";
-import Comment from "../../components/comment/Comment";
 
 const HomePage = () => {
-  const test = [
-    {
-      profile_image: "https://picsum.photos/200",
-      username: "username",
-      comment: "ttett",
-      date: "25.10.2023",
-    },
-    {
-      profile_image: "https://picsum.photos/200",
-      username: "usernfdsfdsfdsame",
-      comment:
-        "esttesttesttestesttesttesttesttesttesdfdsfsttesttsttestttesttesdfdsfsttesttsttestt",
-      date: "25.10.2023",
-    },
-    {
-      profile_image: "https://picsum.photos/200",
-      username: "fdsfdsfds",
-      comment:
-        "esttesttesttestesttesttesttesttesttesttesttsttestttesttesttesttsttestt",
-      date: "25.10.2023",
-    },
-  ];
-  return (
-    <List list={test} CardComponent={Comment} flexDirection="column-reverse" />
-  );
+  return <div>Home Page</div>;
 };
 
 export default HomePage;
