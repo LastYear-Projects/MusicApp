b9863731fc2b91d315a94967ad69474e2cead10c
Idan Asayag <idanasayag0@gmail.com>
2023-12-15 15:02:39 +0200

    create basic commend

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import {
+  Avatar,
+  Typography,
+  Box,
+  Paper,
+  Button,
+  useTheme,
+} from "@mui/material";
 
-const HomePage = () => {
-  return <div>Home Page</div>;
+const Comment = ({
+  profile_image = "https://picsum.photos/200",
+  username = "username",
+  comment = "esttesttesttesttesttesttesttsttestt",
+  date = "25.10.2023",
+}) => {
+  const theme = useTheme();
+
+  return (
+    <Paper
+      elevation={3}
+      style={{
+        padding: "10px",
+        marginBottom: "10px",
+        wordBreak: "break-all",
+        maxWidth: "35rem",
+        minWidth: "15rem",
+      }}
+    >
+      <Box display="flex" flexDirection="column">
+        <Box display="flex" alignItems="center">
+          <Avatar src={profile_image} alt="Avatar" />
+          <Box marginLeft="1rem">
+            <Typography variant="subtitle1" fontWeight="bold">
+              {username}
+            </Typography>
+            <Typography variant="body1">{comment}</Typography>
+          </Box>
+        </Box>
+
+        <Box display="flex" alignItems="center" marginTop={1}>
+          <Button size="small" variant="text" color="error" onClick={() => {}}>
+            Remove
+          </Button>
+          <Typography variant="caption" color="textSecondary" marginLeft="auto">
+            {date}
+          </Typography>
+        </Box>
+      </Box>
+    </Paper>
+  );
 };
 
-export default HomePage;
+export default Comment;
