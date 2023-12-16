3b12f19307b05466d37169402dc73c4abf42ad98
Idan Asayag <idanasayag0@gmail.com>
2023-12-16 21:54:51 +0200

    fix the loader and fetch the data from db

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,22 @@
+import { Box } from "@mui/material";
 import React from "react";
-
+import useFetch from "../../hooks/useFetch";
+import List from "../../components/list/List";
+import Loader from "../../components/loader/loader";
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const {
+    data: songs,
+    error,
+    isLoading,
+  } = useFetch("http://localhost:6969/songs");
+  //TODO : send the data to context/redux, and handle error.
+  return (
+    <Box>
+      <Loader isLoading={isLoading}>
+        <List list={songs} />
+      </Loader>
+    </Box>
+  );
 };
 
 export default HomePage;
