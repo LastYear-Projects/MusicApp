ca7d67a0b0fa61eef0fc32016bd5409fb66ea293
YuvalOfek1 <yuvalofek97@gmail.com>
2023-12-19 16:16:02 +0200

    fixed a bug in invalid token checking

--- backend/src/validations/index.js
+++ backend/src/validations/index.js
@@ -13,9 +13,9 @@ const checkToken = (req, res, next) => {
         if (err) {
             return res.status(403).json({message: "Invalid token"});
         }
-        
+        next();
     });
-    next();
+    
 }
 
 //checks if the user has filled all the fields correctly
