c75f07b32de4e6a4f10f9f36f7dbb015023780b0
Idan Asayag <idanasayag0@gmail.com>
2023-12-24 11:50:54 +0200

    some changes for chat

--- backend/src/utils/sockets.js
+++ backend/src/utils/sockets.js
@@ -5,21 +5,24 @@ const handleClient = (io) => {
     console.log("New WebSocket Connection...");
 
     // Listen for incoming chat messages
-    socket.on("message", (message) => {
-      console.log("message: ", message);
+    socket.on("message", (messageObject) => {
+      console.log("messageObject: ", messageObject);
 
       // Sanitize the message before broadcasting
-      const sanitizedMessage = xss.escapeHtml("hello from server!");
+      const sanitizedMessage = xss.escapeHtml(messageObject.message);
 
       // Broadcast the sanitized message to all connected clients
-      io.emit("message", sanitizedMessage);
+      io.emit("message", {
+        message: sanitizedMessage,
+        userDetails: messageObject.userDetails,
+      });
     });
 
-    
     socket.on("disconnect", () => {
       io.emit("message", "A user has left the chat");
       console.log("Client has closed the socket");
     });
   });
 };
+
 module.exports = { handleClient };
