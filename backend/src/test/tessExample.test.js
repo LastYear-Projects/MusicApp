6ae051ead78907cb320ba7a90e458a4e24df05e7
DanDavidovich <davidovich.dan@gmail.com>
2024-01-01 12:56:25 +0200

    small changes

--- backend/src/test/tessExample.test.js
+++ backend/src/test/tessExample.test.js
@@ -10,11 +10,11 @@ afterAll(done => {
   done();
 });
 describe('this is the first test case', () => {
-    test("this is the first test case", () => {
+    it("this is the first test case", () => {
         const temp=2;
         expect(temp).toBe(2);
     })
-    test("this is the second test case should pass", () => {
+    it("this is the second test case should pass", () => {
         const temp=2
         expect(temp).not.toBe(3)
 
