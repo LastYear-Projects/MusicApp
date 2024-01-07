92769389307017f7cd4dc6a5ae52f068f71aa7cd
Idan Asayag <idanasayag0@gmail.com>
2024-01-07 19:59:01 +0200

    notfoundpage -> tsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ import CartPage from "./pages/CartPage/CartPage.tsx";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import SongPage from "./pages/SongPage/SongPage";
 import UserProfile from "./pages/UserPage/UserPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.tsx";
 import { gapi } from "gapi-script";
 import PrivateRoutes from "./components/privateRoutes/PrivateRoutes.tsx";
 
